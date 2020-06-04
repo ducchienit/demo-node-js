16fe489f29ddc25b5299444b627e955709018b8d
ducchienit <ducchienit2k1@gmail.com>
2020-06-04 10:20:42 +0700

    jhgjh

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+
+
 var express = require('express');
 var userRoute = require('./routes/user.route');
 var authRote = require('./routes/auth.route');
+var productRoute = require('./routes/product.route');
+var cartRoute = require('./routes/cart.route');
 var cookieParser = require('cookie-parser');
+var transferRoute = require('./routes/transfer.route');
+var apiProductRoute = require('./api/routes/product.route');
+var csurf = require('csurf');
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL);
 
 var authMiddleware = require('./middlewares/auth.middleware');
+var sessionMiddleware = require('./middlewares/session.middleware');
 
-var port = 3000; 
+var port = 8888; 
 
 var app = express();
 app.set('view engine', 'pug');
@@ -13,7 +25,9 @@ app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser("fdffffds13123"));
+app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(sessionMiddleware);
+app.use(csurf({ cookie: true }));
 
 app.use(express.static('public'));
 
@@ -25,10 +39,14 @@ app.get('/', function(request, response){
 
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRote);
+app.use('/products', productRoute);
+app.use('/cart', cartRoute);
+app.use('/transfer',authMiddleware.requireAuth, transferRoute);
+app.use('/api/products', apiProductRoute);
 
 app.listen(port, function() {
     console.log('Server listening on port ' + port);
 
 });
 
-
+// This is index file
