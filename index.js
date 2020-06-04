c31abf136409ca5bd146114f981875924b043e60
ducchienit <ducchienit2k1@gmail.com>
2020-06-04 11:14:45 +0700

    index.js using lowdb

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ var productRoute = require('./routes/product.route');
 var cartRoute = require('./routes/cart.route');
 var cookieParser = require('cookie-parser');
 var transferRoute = require('./routes/transfer.route');
-var apiProductRoute = require('./api/routes/product.route');
 var csurf = require('csurf');
-var mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URL);
 
 var authMiddleware = require('./middlewares/auth.middleware');
 var sessionMiddleware = require('./middlewares/session.middleware');
@@ -27,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(sessionMiddleware);
+app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use(csurf({ cookie: true }));
 
 app.use(express.static('public'));
@@ -37,12 +34,10 @@ app.get('/', function(request, response){
     });
 });
 
-app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRote);
 app.use('/products', productRoute);
 app.use('/cart', cartRoute);
 app.use('/transfer',authMiddleware.requireAuth, transferRoute);
-app.use('/api/products', apiProductRoute);
 
 app.listen(port, function() {
     console.log('Server listening on port ' + port);
